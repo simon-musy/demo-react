c67628f9139c12ad36906ace258734e4a9f57c8b
Simon <Simon>
2017-06-11 14:08:24 +0200

    1-8

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+// This component handles the App template used on every page.
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <Header/>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default App;
